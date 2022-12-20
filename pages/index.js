9dfea1d1f53c1277f642f00cda52ef38fb3138ed
najmul-eimon <najmul.eimon@intelli.global>
2022-12-20 18:45:10 +0600

    font update

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,10 @@ export default function Home() {
     <>
       <Head>
         <title>Real Estate | Home</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </Head>
       <Banner/>
       <Achievement/>
