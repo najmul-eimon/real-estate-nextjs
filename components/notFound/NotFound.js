b4eb9c4df69af2584e90d3c5a980300b348d26c7
najmul-eimon <najmul.eimon@intelli.global>
2022-12-28 12:02:05 +0600

    not found page update

--- components/notFound/NotFound.js
+++ components/notFound/NotFound.js
@@ -1,18 +1,17 @@
 import Link from 'next/link';
-import image from '../../public/not-found1.png';
 import Image from 'next/image';
 
 const NotFound = () => {
 
   return (
     <main>
-      <section className='not-found'>
+      <section className="not-found">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <Image src={image} alt="Not Found Image"/>
+              <Image src="/not-found.png" alt="Not Found Image" width={1296} height={606}/>
               <div className="d-flex align-items-center justify-content-center">
-                <Link href="/">Back to Home</Link>
+                <Link href="/" className='btn-style'>Back to Home</Link>
               </div>
             </div>
           </div>
